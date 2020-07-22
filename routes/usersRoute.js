9e05df1938a9136ecbcc22f081622a2898332665
Moulik Aggarwal <qwertymoulik@gmail.com>
2020-07-23 01:26:18 +0530

    get all users

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -5,4 +5,6 @@ const express = require('express'),
 
 router.post('/:id', (req, res) => CRUD.updateById(req.params.id, req.body, Model, res));
 
+router.get('/', (req, res) => CRUD.getAll(req, Model, res));
+
 module.exports = router;
