f66620e217baf1fedd7c1a90ab275115282dff59
Moulik Aggarwal <qwertymoulik@gmail.com>
2020-09-03 01:51:04 +0530

    Updated Dashboard Orders
    Signed-off-by: Moulik Aggarwal <qwertymoulik@gmail.com>

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
     let offer = await offers.find({ status: 1 });
     let service = await services.find({ status: 1 });
     let blog = await blogs.find({ status: 1 });
-    let order = await orders.find({ status: 1 });
+    let order = await orders.find();
     let category = await categories.find({status: 1});
     let products = await items.find({status: 1});
 
